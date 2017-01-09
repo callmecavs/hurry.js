23745608c7e0bac48bafa417c4685ef49dd03635
callmecavs <callmecavs@gmail.com>
2017-01-08 20:52:05 -0500

    fix linting errors

--- src/hurry.js
+++ src/hurry.js
@@ -1,13 +1,13 @@
 // convert function arguments object to array
 // slice prevents optimization: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
 
-function toArray(args) {
+const toArray = args => {
   const max = args.length
 
-  let array   = []
+  let array = []
   let current = 0
 
-  while(current < max) {
+  while (current < max) {
     array.push(args[current])
     current++
   }
@@ -18,19 +18,21 @@ function toArray(args) {
 // an optimized curry function
 // calls the original function when all arguments have been recieved, collecting them until then
 
-export default function hurry(original) {
+const hurry = original => {
   // cache number of arguments passed to original function
   const target = original.length
 
-  return function collector() {
+  return function collector () {
     // collect current arguments (future arguments are added to this)
     const args = toArray(arguments)
 
     // check number of arguments
     return args.length >= target
-      ? original.apply(null, args)                                          // if we have them all, call original function
-      : function() {                                                        // otherwise, add new arguments to current ones
-          return collector.apply(null, args.concat(toArray(arguments)))
-        }
+      ? original.apply(null, args)                                      // if we have them all, call original function
+      : function () {                                                   // otherwise, add new arguments to current ones
+        return collector.apply(null, args.concat(toArray(arguments)))
+      }
   }
 }
+
+export default hurry
